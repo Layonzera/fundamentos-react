60812d6911e43abd9b9fabaa87d71bebed76596c
Adryann Layon <adryann.layon7@gmail.com>
2023-12-28 20:48:58 -0300

    :test_tube: test: learn how use dynamic expressions in react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+const category = 'Posts da semana';
+
 export function App() {
   return (
     <>
-      <h1>Componente App</h1>
-      <h2>Subtitulo</h2>
+      <h1>Adryann's Blog</h1>
+      <h2>{category}</h2>
     </>
   );
 }
