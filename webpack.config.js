f858eb101daa747460b8decfb7c4c34d1166b09b
Adryann Layon <adryann.layon7@gmail.com>
2023-12-28 15:17:30 -0300

    :wrench: chore: config loader in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,5 +14,14 @@ module.exports = {
       template: path.resolve(__dirname, 'public', 'index.html'),
     }),
     new CleanWebpackPlugin(),
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ]
+  }
 };
