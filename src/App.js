1f54177943794be8b94e2510fcf00a8a29b159a0
Adryann Layon <adryann.layon7@gmail.com>
2023-12-29 20:26:05 -0300

    :test_tube: test: learn how render list of components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import React from "react";
 import { Post } from "./Post";
 import { Header } from "./Header";
 
+const posts = [
+  { title: 'Title#01', subtitle: 'Sub#01', likes: 20 },
+  { title: 'Title#02', subtitle: 'Sub#02', likes: 10 },
+  { title: 'Title#03', subtitle: 'Sub#03', likes: 50 },
+]
 
 export function App() {
   return (
@@ -13,29 +18,18 @@ export function App() {
 
       <hr />
 
-      <Post 
-        likes={20}
-        post={{
-          title: 'Titulo da noticia 01',
-          subtitle: 'Subtitulo da noticia 01'
-        }}
-      />
-
-      <Post 
-        likes={10}
-        post={{
-          title: 'Titulo da noticia 02',
-          subtitle: 'Subtitulo da noticia 02'
-        }}
-      />
-
-      <Post 
-        likes={50}
-        post={{
-          title: 'Titulo da noticia 03',
-          subtitle: 'Subtitulo da noticia 03'
-        }}
-      />
+      {
+        posts.map((post) => (
+          <Post 
+            key={post.title}
+            likes={post.likes}
+            post={{
+              title: post.title,
+              subtitle: post.subtitle
+            }}
+          />
+        ))
+      }
     </>
   );
 }
