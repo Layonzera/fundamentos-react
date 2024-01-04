f8b592ad9773875cefc68caa151f57636027af67
Adryann Layon <adryann.layon7@gmail.com>
2024-01-03 21:43:14 -0300

    :test_tube: test: learn how style component about props data

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,10 +7,10 @@ import { Title } from './styles';
 
 export function App() {
   const [posts, setPosts] = useState([
-    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: false },
-    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10, read: true },
-    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50, read: false },
-    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 50, read: true },
+    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: false, removed: false },
+    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10, read: true, removed: false },
+    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50, read: false, removed: false },
+    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 50, read: true, removed: false },
   ]);
   
   function handleRefresh() {
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -3,25 +3,22 @@ import PropTypes from 'prop-types';
 
 import { PostHeader } from "./PostHeader";
 
-import { Rate, Subtitle } from './styles';
+import { Container, Rate, Subtitle } from './styles';
 
 export function Post(props) {
   return (
-    <>
-      <article>
-        <PostHeader
-          onRemove={props.onRemove}
-          post={{
-            id: props.post.id,
-            title: props.post.title,
-            read: props.post.read
-          }}
-        />
-        <Subtitle>{props.post.subtitle}</Subtitle>
-        <Rate>Media: {props.post.likes / 2}</Rate>
-      </article>
-      <br />
-    </>
+    <Container removed={props.post.read}>
+      <PostHeader
+        onRemove={props.onRemove}
+        post={{
+          id: props.post.id,
+          title: props.post.title,
+          read: props.post.read
+        }}
+      />
+      <Subtitle>{props.post.subtitle}</Subtitle>
+      <Rate>Media: {props.post.likes / 2}</Rate>
+    </Container>
   )
 }
 
